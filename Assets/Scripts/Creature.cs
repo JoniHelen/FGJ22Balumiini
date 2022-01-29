e434ff18f48b38b2ba5e1f85f0e748d1d743a006
tstkokki <60537602+tstkokki@users.noreply.github.com>
2022-01-29 10:48:36 +0200

    test ui

--- Assets/Scripts/Creature.cs
+++ Assets/Scripts/Creature.cs
@@ -4,10 +4,29 @@ using UnityEngine;
 
 public class Creature : MonoBehaviour
 {
+
+    // define the state of being selected
+    enum State
+    {
+        Idle,
+        Selected,
+        Wait
+    }
+
+    public int Move { get; set; }
+
+
+    State myState;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Move = 3;
+    }
+
+    public void GetState()
+    {
+        Debug.Log($"{myState}, move: {Move}");
     }
 
 }

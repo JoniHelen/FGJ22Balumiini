a89f6ecfbbc95e07fa9c680d4d29fba94ee139ab
tstkokki <60537602+tstkokki@users.noreply.github.com>
2022-01-30 14:49:47 +0200

    close game. movement trail

--- Assets/Scripts/Battle.cs
+++ Assets/Scripts/Battle.cs
@@ -22,13 +22,33 @@ public class Battle : MonoBehaviour
 
     public void CombatRound(Creature attacker, Creature defender)
     {
+        DisplayForecast(attacker, defender);
+
+        StartCoroutine(Turn(attacker, defender));
+
+    }
+
+    private void DisplayForecast(Creature attacker, Creature defender)
+    {
         //display HP
-        Attacker.text = 
+        Attacker.text =
             $"HP: {attacker.HP}/{attacker.maxHP}\nATK: {attacker.Strength}\nDEF: {attacker.PhysicalDefense}";
-        Defender.text = 
+        Defender.text =
         $"HP: {defender.HP}/{defender.maxHP}\nATK: {defender.Strength}\nDEF: {defender.PhysicalDefense}";
 
-        Forecast.text = $"DMG\n{attacker.Strength-defender.PhysicalDefense} VS {defender.Strength-attacker.PhysicalDefense}";
+        Forecast.text = $"DMG\n{attacker.Strength - defender.PhysicalDefense} VS {defender.Strength - attacker.PhysicalDefense}";
+    }
+
+    WaitForSeconds turn = new WaitForSeconds(0.5f);
+
+    IEnumerator Turn(Creature attacker, Creature defender)
+    {
+        yield return new WaitForSeconds(1);
+        defender.TakeDamage(Mathf.Max(attacker.Strength - defender.PhysicalDefense, 0));
+        DisplayForecast(attacker, defender);
+        yield return turn;
+        attacker.TakeDamage(Mathf.Max(defender.Strength - attacker.PhysicalDefense, 0));
+        DisplayForecast(attacker, defender);
     }
 
 }

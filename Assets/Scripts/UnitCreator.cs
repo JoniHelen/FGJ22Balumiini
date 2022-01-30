bb7af2d91bdee038a30e8fb810caba5d6766d7f6
JoniHelen <djohe015@edu.xamk.fi>
2022-01-30 13:48:11 +0200

    update from main

--- Assets/Scripts/UnitCreator.cs
+++ Assets/Scripts/UnitCreator.cs
@@ -13,9 +13,12 @@ public class UnitCreator : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+        StatSheet statSheet = new StatSheet(10, 10, 5, 5, 3, 2, false);
         for (int i = 0; i < numberOfUnits; i++)
         {
-            Creature _unit = Instantiate(unitPrefab, Vector3Int.right * i + Vector3Int.up*yOffset, transform.rotation);
+            Creature _unit = Instantiate(unitPrefab, Vector3Int.right * i + Vector3Int.up * yOffset, transform.rotation);
+
+            _unit.Init(statSheet);
             _unitList.AddUnit(_unit);
         }
 

6b1ae9ea7fcd4c80aab03e989157d032beabfa8c
tstkokki <60537602+tstkokki@users.noreply.github.com>
2022-01-30 18:16:32 +0200

    start game gallery button

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+
 [RequireComponent(typeof(Button))]
 public class StartGame : MonoBehaviour
 {
